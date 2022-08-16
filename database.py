33e339f15fdd7d6bcfd84c060eb98b71669e334e
Paige Inia <iniapaige@gmail.com>
2022-08-16 23:30:24 +1200

    Enhanced application so a user can change their password - created new file in templates directory called ‘edit-user-password.html’; created a new users.py file within the routers directory; added ability to verify password and then change the password. STILL UNABLE TO LOGIN - MUST BE FIXED!

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Test1234@127.0.0.1:3306/todoapp"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Test1234@localhost/TodoApplicationDatabase"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
